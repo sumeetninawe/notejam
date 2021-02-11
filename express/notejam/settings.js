9e782639bf078e20d36b58583344fbe6894ae0e2
Sumeet <me@sumeet.life>
2021-02-12 00:14:57 +0100

    Update settings.js

--- express/notejam/settings.js
+++ express/notejam/settings.js
@@ -1,7 +1,7 @@
 var settings = {
   development: {
-    db: "notejam_dev.db",
-    dsn: "sqlite://mnt/efs/fs1/notejam_dev.db"
+    db: "/mnt/efs/fs1/notejam_dev.db",
+    dsn: "sqlite://notejam_dev.db"
   },
   test: {
     db: "notejam_test.db",
